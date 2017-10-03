86b7322b308eae0206b6f0fba0d9e10347b76cd6
d35k <goktughatipoglu@yandex.com>
2017-10-04 01:11:17 +0300

    * Turkish Language Support

---
+++ resources/lang/tr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Sistemimizde belirtilen bilgilerde bir kayıt bulamadık.',
+    'throttle' => 'Çok fazla giriş denemesi yaptınız, lütfen :seconds saniye sonra tekrar deneyiniz.',
+
+];
